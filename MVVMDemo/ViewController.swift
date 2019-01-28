250cd683d3fa22a6d710f7fb788ce95edab391be
Jordan Lin <mocha5011@gmail.com>
2019-01-28 15:30:58 +0800

    Initial Commit

---
+++ MVVMDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMDemo
+//
+//  Created by Jordan Lin on 2019/1/28.
+//  Copyright © 2019 Jordan Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
